de4c1086be68325827e1b522babac26744a1c2d5
elmuradhasan <hsnlimurad540@gmail.com>
2025-05-03 15:38:48 +0400

    added form

--- src/features/UserSearch/index.tsx
+++ src/features/UserSearch/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Input, Button } from '@chakra-ui/react';
+import {
+  Input,
+  Button,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
 import { schema } from '../../schema/userValidationSchema';
-import { useQuery } from 'react-query';
 import { getUserByUsername } from '@services/userService';
+import { LuUser } from 'react-icons/lu';
 
 const UserSearchForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
   const onSubmit = async (data: any) => {
-    // API sorğusu burada ediləcək
     try {
       const user = await getUserByUsername(data.username);
       console.log(user);
@@ -22,11 +34,29 @@ const UserSearchForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-    
-      <Button mt={4} colorScheme="teal" type="submit">
-        Search
-      </Button>
+    <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%' }}>
+      <Stack spacing={4} maxW="400px" mx="auto">
+        <Controller
+          name="username"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <FormControl isInvalid={!!errors.username}>
+              <FormLabel>İstifadəçi adı</FormLabel>
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <LuUser color="gray" />
+                </InputLeftElement>
+                <Input {...field} placeholder="İstifadəçi adını daxil et" />
+              </InputGroup>
+              <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        <Button type="submit" colorScheme="blue" w="full">
+          Axtar
+        </Button>
+      </Stack>
     </form>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
-import UserSearchForm from '@features/UserSearch'
-import React from 'react'
+import { Box, HStack } from "@chakra-ui/react";
+import UserSearchForm from "@features/UserSearch";
+import React from "react";
 
 function Home() {
   return (
- <>
- <UserSearchForm/>
- </>
-  )
+    <Box p={4} bg="gray.100" minH="100vh" justifyContent="center" alignItems="center"  width={"100%"}>
+        <UserSearchForm />
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
